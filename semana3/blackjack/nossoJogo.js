692d408cec0e34781462cdaef68cb907878da861
unknown <you@example.comwellington.acardoso18@gmail.com>
2020-04-03 16:12:42 -0300

    Arquivos do projeto semana 3

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,20 @@
+
+ /* EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta' */
+ 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+
+
+
+
+
+ alert('Bem vindo ao jogo de Blackjack!')
+ if(confirm('Quer iniciar uma nova rodada?')){
+  
+    console.log('O jogo começou')/* teste  do botão confirm*/
+
+ }else{
+    console.log('o Jogo acabou')
+ }
