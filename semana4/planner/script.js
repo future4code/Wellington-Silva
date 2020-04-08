11eaf5530d0711b05f4daf18e3d27b44bd771c3d
unknown <you@example.comwellington.acardoso18@gmail.com>
2020-04-07 22:27:45 -0300

    alterando arquivos3

--- semana4/planner/script.js
+++ semana4/planner/script.js
@@ -33,24 +33,3 @@ function addItem() {
 	document.getElementById('task').value = "";
 }
 
-function popOffItem() {
-	let checked_list = document.querySelectorAll('#check-box');
-	let checked_list_item = document.querySelectorAll('.list-item');
-	let lineBreaks = document.querySelectorAll('br');
-	for (i=0; i < checked_list.length; i++) {
-		if (checked_list[i].checked==true) {	  
-			//Removing the Item from the Document
-			let checkbox_elemt = checked_list[i];
-			let item = checked_list_item[i];
-			let removeBreakLine = lineBreaks[i];
-			let parent1 = checkbox_elemt.parentNode;
-			let parent2 = item.parentNode;
-			let parent3 = removeBreakLine.parentNode;
-			parent1.removeChild(checkbox_elemt);
-			parent2.removeChild(item);
-			parent3.removeChild(removeBreakLine);
-		}else {
-			continue;
-		}
-	}
-}
